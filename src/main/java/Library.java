6c0fa69b9cb2a1e58ec34f51a70f4e66b842368c
KevSeenan <kev.seenan@gmail.com>
2019-09-24 18:41:30 +0100

    Adds HashMap for tracking by genre still working on function for this

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
 
     private String name;
     private int capacity;
     private ArrayList<Book> books;
+    private HashMap<String, Integer> genreTracker;
 
     public Library(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
         this.books = new ArrayList<Book>();
+        this.genreTracker = new HashMap<String, Integer>();
     }
 
     public int bookCount() {
@@ -19,6 +22,18 @@ public class Library {
     public void addBook(Book book) {
         if(bookCount() < capacity) {
             this.books.add(book);
+
+            if(!this.genreTracker.containsKey(book)){
+                Integer count = 0;
+                this.genreTracker.get(book);
+            } else {
+               Integer count = 1;
+               this.genreTracker.get(book);
+            }
         }
+    }
+
+    public int genreCount() {
+        return this.genreTracker.size();
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -7,12 +7,14 @@ import static org.junit.Assert.assertNotNull;
 public class LibraryTest {
 
     Library library;
-    Book book;
+    Book book, book2, book3;
 
     @Before
     public void setUp(){
         library = new Library("Unseen University Library", 2);
         book = new Book ("Mort", "Terry Pratchett", "Fantasy");
+        book2 = new Book ("Jingo", "Terry Pratchett", "Fantasy");
+        book3 = new Book ("Interesting Times", "Terry Pratchett", "Fantasy");
     }
 
     @Test
@@ -46,8 +48,23 @@ public class LibraryTest {
 
         //Then I can add a book
         library.addBook(book);
-        library.addBook(book);
-        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
         assertEquals(2, library.bookCount());
+    }
+
+    @Test
+    public void canTrackNumberOfBooksByGenre(){
+        //Given I have a library
+        assertNotNull(library);
+        //AND I have a book
+        assertNotNull(book);
+        //When the library has books
+        library.addBook(book);
+        library.addBook(book2);
+        library.addBook(book3);
+        //Then I can count books by genre
+        assertEquals(3, library.genreCount());
+
     }
 }
